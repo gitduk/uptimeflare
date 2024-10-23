ca9dede53177d8e17b75a3d85fec325d71b920d5
gitduk <wukaigee@gmail.com>
2024-10-23 12:02:48 +0800

    Update uptime.config.ts

--- uptime.config.ts
+++ uptime.config.ts
@@ -1,11 +1,11 @@
 const pageConfig = {
   // Title for your status page
-  title: "lyc8503's Status Page",
+  title: "wukaige's Status Page",
   // Links shown at the header of your status page, could set `highlight` to `true`
   links: [
-    { link: 'https://github.com/lyc8503', label: 'GitHub' },
-    { link: 'https://blog.lyc8503.net/', label: 'Blog' },
-    { link: 'mailto:me@lyc8503.net', label: 'Email Me', highlight: true },
+    { link: 'https://github.com/gitduk', label: 'GitHub' },
+    { link: 'https://blog.wukaige.com/', label: 'Blog' },
+    { link: 'mailto:wukaigee@gmail.com', label: 'Email Me', highlight: true },
   ],
 }
 
@@ -37,7 +37,7 @@ const workerConfig = {
       // [OPTIONAL] headers to be sent
       headers: {
         'User-Agent': 'Uptimeflare',
-        Authorization: 'Bearer YOUR_TOKEN_HERE',
+        Authorization: 'Bearer 9icz26zXUdxfDB1gSnSn-ersceAASROhwErpJdsE',
       },
       // [OPTIONAL] body to be sent
       body: 'Hello, world!',
